b50dfde9c0c7d35d42c790a4005419b0e0ab864a
Sari Sabban <sari.sabban@gmail.com>
2023-03-04 17:08:21 -0500

    Update MolecularTetris.py

--- MolecularTetris.py
+++ MolecularTetris.py
@@ -547,7 +547,7 @@ def RL(epochs=1, play=False, filename='policy.pth'):
 
 def main():
 	if   args.play:     play()
-	elif args.rl_train: RL(epochs=100)
+	elif args.rl_train: RL(epochs=1000)
 	elif args.rl_play:  RL(epochs=0, play=True, filename=sys.argv[2])
 
 if __name__ == '__main__': main()
