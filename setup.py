90b6e36c0bc39b39b449fda3fa9c48f79ad4530f
acresearch <sari.sabban@gmail.com>
2023-04-04 00:56:44 -0400

    pip install adjustment

--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@ setuptools.setup(
 	url='https://github.com/sarisabban/MolecularTetris',
 	project_urls={'Bug Tracker':'https://github.com/sarisabban/MolecularTetris/issues'},
 	license='GPL-2.0',
-	packages=['MolecularTetris'],
+	packages=['moleculartetris'],
 	install_requires=['numpy', 'scipy', 'gym', 'torch', 'tianshou'])
