fe8487e8477cdf3bacd4519b9c5fe5778270f7da
Sari Sabban <sari.sabban@gmail.com>
2023-05-05 14:09:05 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import datetime
 import matplotlib.pyplot as plt
 
-updates, steps, returns, SD, A_loss, C_loss, E_loss, loss, KL, clip, time = [], [], [], [], [], [], [], [], [], [], []
+updates, steps, returns, SD, A_loss, C_loss, E_loss, loss, KL, clip, var = [], [], [], [], [], [], [], [], [], [], []
 filename = 'train.log'
 with open(filename) as f:
 	for line in f:
@@ -16,46 +16,39 @@ with open(filename) as f:
 		loss.append(   float(line[17].replace(',', '')))
 		KL.append(     float(line[19].replace(',', '')))
 		clip.append(   float(line[21].replace(',', '')))
-		h = int(line[24:][-1].split(':')[0])
-		m = int(line[24:][-1].split(':')[1])
-		s = int(line[24:][-1].split(':')[2])
-		if line[24:][1] == 'days,': d = int(line[24:][0])
-		else:                       d = 0
-		T = datetime.timedelta(days=d, hours=h, minutes=m, seconds=s)
-		T = T.total_seconds()
-		time.append(T)
+		var.append(    float(line[23].replace(',', '')))
 
 fig, axs = plt.subplots(4, 2)
 axs[0, 0].plot(steps, returns)
 axs[0, 0].set_title('Returns')
 axs[0, 0].set(xlabel='Steps', ylabel='Returns')
 
-axs[0, 1].plot(steps, loss)
+axs[0, 1].plot(updates, loss)
 axs[0, 1].set_title('Final Loss')
-axs[0, 1].set(xlabel='Steps', ylabel='Loss')
+axs[0, 1].set(xlabel='Updates', ylabel='Loss')
 
-axs[1, 1].plot(steps, A_loss)
+axs[1, 1].plot(updates, A_loss)
 axs[1, 1].set_title('Actor loss')
-axs[1, 1].set(xlabel='Steps', ylabel='Loss')
+axs[1, 1].set(xlabel='Updates', ylabel='Loss')
 
-axs[2, 1].plot(steps, C_loss)
+axs[2, 1].plot(updates, C_loss)
 axs[2, 1].set_title('Critic loss')
-axs[2, 1].set(xlabel='Steps', ylabel='Loss')
+axs[2, 1].set(xlabel='Updates', ylabel='Loss')
 
-axs[3, 1].plot(steps, E_loss)
+axs[3, 1].plot(updates, E_loss)
 axs[3, 1].set_title('Entropy loss')
-axs[3, 1].set(xlabel='Steps', ylabel='Loss')
+axs[3, 1].set(xlabel='Updates', ylabel='Loss')
 
-axs[1, 0].plot(steps, KL)
+axs[1, 0].plot(updates, KL)
 axs[1, 0].set_title('KL aproximation')
-axs[1, 0].set(xlabel='Steps', ylabel='KL')
+axs[1, 0].set(xlabel='Updates', ylabel='Ratio')
 
-axs[2, 0].plot(steps, clip)
+axs[2, 0].plot(updates, clip)
 axs[2, 0].set_title('Clip fraction')
-axs[2, 0].set(xlabel='Steps', ylabel='Fraction')
+axs[2, 0].set(xlabel='Updates', ylabel='Fraction')
 
-axs[3, 0].plot(steps, time)
-axs[3, 0].set_title('Time')
-axs[3, 0].set(xlabel='Steps', ylabel='Seconds')
+axs[3, 0].plot(updates, var)
+axs[3, 0].set_title('Explained variance')
+axs[3, 0].set(xlabel='Updates', ylabel='variance')
 
 plt.show()
