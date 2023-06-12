893af06a7631b1c997baaa6e8ac87f11fb35cc6d
Sari Sabban <sari.sabban@gmail.com>
2023-06-12 13:06:18 +0300

    Update MiraMar.py

--- MiraMar.py
+++ MiraMar.py
@@ -430,9 +430,8 @@ class MiraMar():
 		# Path reward
 		if self.i != 0: R = -(1/9) * d**2 + 1
 		# Target reward
-		if   hit == 1: R += (-1/33)*SC_size + 1           # Hit
-		elif hit == 0 or hit == 2: R += (-1/33)*SC_size   # Far or no rotamers
-		elif hit == 3: R -= 1                             # Miss
+		if    hit == 1: R += (-1/33)*SC_size + 1 # Hit
+		else:           R += (-1/33)*SC_size     # Else
 		###########################
 		######## Features #########
 		###########################
